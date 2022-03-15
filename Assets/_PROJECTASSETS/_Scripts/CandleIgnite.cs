34d76fc156a904cfcf6a77baf4efd5440532627a
ericssonnumber2 <eric.staici@ue-germany.de>
2022-03-15 17:11:51 +0100

    Added candle logic

---
+++ Assets/_PROJECTASSETS/_Scripts/CandleIgnite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleIgnite : MonoBehaviour
+{
+    [SerializeField] GameObject SelectedCandle;
+    [SerializeField] ParticleSystem CandleInitVFX;
+    [SerializeField] ParticleSystem CandleVFX;
+    public bool isON;
+    RaycastHit hit;
+    public void IgniteSelectedCandle()
+    {
+        SelectedCandle.GetComponentInChildren<ParticleSystem>().Play();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(isON == false)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                IgniteSelectedCandle();
+                isON = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+       
+    }
+}
